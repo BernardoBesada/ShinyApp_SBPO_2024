2e80b11fe5d7798d94aa13070911b892220f6728
BernardoBesada <bernardobesada@gmail.com>
2025-01-21 21:10:49 -0300

    Fixed methods names

--- methods.R
+++ methods.R
@@ -26,7 +26,7 @@ geomean <- function(arr){
     return(exp(mean(log(arr))))
 }
 
-MABAC <- function(mat){
+CRITIC <- function(mat){
     stds <- apply(mat, 2, sd)
     cors_matrix <- spearman_cor_matrix(mat)
     conflict_measure <- apply(1-cors_matrix, 2, sum)
@@ -34,7 +34,7 @@ MABAC <- function(mat){
     return(info/sum(info))
 }
 
-CRITIC <- function(mat, weights){
+MABAC <- function(mat, weights){
     weighted <- sweep((mat + 1), 2, weights, "*")
     border <- apply(weighted, 2, geomean)
     distance <- sweep(weighted, 2, border, "-")
@@ -45,8 +45,8 @@ CRITIC <- function(mat, weights){
 calculate_scores <- function(num_values, normalization){
     num_values[, !normalization] <- apply(num_values[, !normalization, drop = FALSE], 2, min_max)
     num_values[, normalization] <- apply(num_values[, normalization, drop = FALSE], 2, reverse_min_max)
-    weights <- MABAC(num_values)
-    scores <- CRITIC(num_values, weights)
+    weights <- CRITIC(num_values)
+    scores <- MABAC(num_values, weights)
 
     return(list("Criteria" = colnames(num_values), "Scores" = scores, "Weights" = weights))
 }
